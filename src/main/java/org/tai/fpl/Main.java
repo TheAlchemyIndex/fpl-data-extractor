fa841455f53454a6ecddebd8d02aae33202f469c
TheAlchemyIndex <adam.vaughan@hotmail.co.uk>
2023-04-09 20:25:41 +0100

    adding logging

--- src/main/java/org/tai/fpl/Main.java
+++ src/main/java/org/tai/fpl/Main.java
@@ -14,15 +14,19 @@ import org.tai.fpl.providers.util.constants.JsonKeys;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.tai.fpl.writers.FileWriter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.tai.fpl.Getters.*;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     private static final String TARGET_URL = "https://fantasy.premierleague.com/api/bootstrap-static/";
     private static final String SEASON = "2022-23";
@@ -33,43 +37,57 @@ public class Main {
     private static final String PLAYER_ID_FILENAME = String.format("%splayer_idlist.csv", BASE_FILENAME);
     private static final String GAMEWEEK_FILENAME = String.format("%sgws/gw", BASE_FILENAME);
 
-    /* Will replace print statements with logs */
     public static void main(String[] args) {
         JsonParser jsonParser;
         JSONObject data = new JSONObject();
-        JSONArray currentGameweekData;
+        JSONArray currentGameweekData = new JSONArray();
 
         try {
             UrlConnector urlConnector = new UrlConnector(new URL(TARGET_URL));
             jsonParser = new JsonParser(urlConnector.getResponseString());
             data = jsonParser.parseJsonObject();
         } catch(MalformedURLException malformedURLException) {
-            System.out.println("Invalid target url provided: " + malformedURLException.getMessage());
+            LOGGER.error("Invalid target url provided: " + malformedURLException.getMessage());
         } catch(IOException ioException) {
-            System.out.println("Error connecting to the provided target url: " + ioException.getMessage());
-        } catch(JSONException jsonException) {
-            System.out.println("Error parsing JSON data: " + jsonException.getMessage());
+            LOGGER.error("Error connecting to the provided target url: " + ioException.getMessage());
+        } catch(RuntimeException runtimeException) {
+            if (runtimeException instanceof JSONException) {
+                LOGGER.error("Error parsing JSON data using JsonParser: " + runtimeException.getMessage());
+            } else {
+                LOGGER.error("Error connecting to the provided target url: " + runtimeException.getMessage());
+            }
         }
 
-        ElementProvider elementProvider = new ElementProvider(data.getJSONArray((JsonKeys.ELEMENTS)));
-        JSONArray players = elementProvider.getPlayers();
-
-        EventProvider eventProvider = new EventProvider(data.getJSONArray((JsonKeys.EVENTS)));
-        int currentGameweekNumber = eventProvider.getCurrentGameweek();
-
-        TeamProvider teamProvider = new TeamProvider(data.getJSONArray((JsonKeys.TEAMS)));
-        Map<Integer, String> teams = teamProvider.getTeams();
-
-        Gameweek gameweekProvider = new Gameweek(currentGameweekNumber, players, teams);
+        JSONArray players = new JSONArray();
+        int currentGameweekNumber = 0;
+        Map<Integer, String> teams = new HashMap<>();
         try {
-            currentGameweekData = gameweekProvider.getCurrentGameweekData();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            ElementProvider elementProvider = new ElementProvider(data.getJSONArray((JsonKeys.ELEMENTS)));
+            players = elementProvider.getPlayers();
+
+            EventProvider eventProvider = new EventProvider(data.getJSONArray((JsonKeys.EVENTS)));
+            currentGameweekNumber = eventProvider.getCurrentGameweek();
+
+            TeamProvider teamProvider = new TeamProvider(data.getJSONArray((JsonKeys.TEAMS)));
+            teams = teamProvider.getTeams();
+
+            FileWriter.writeData(elementProvider.getData(), PLAYERS_RAW_FILENAME);
+            FileWriter.writeData(teamProvider.getData(), TEAMS_FILENAME);
+            FileWriter.writeData(players, PLAYER_ID_FILENAME);
+        } catch(IllegalArgumentException | JSONException illegalArgumentException) {
+            if (illegalArgumentException instanceof JSONException) {
+                LOGGER.error("Error parsing JSON data using Provider classes: " + illegalArgumentException.getMessage());
+            } else {
+                LOGGER.error("IllegalArgumentException: " + illegalArgumentException.getMessage());
+            }
         }
 
-        FileWriter.writeData(elementProvider.getData(), PLAYERS_RAW_FILENAME);
-        FileWriter.writeData(teamProvider.getData(), TEAMS_FILENAME);
-        FileWriter.writeData(players, PLAYER_ID_FILENAME);
+        try {
+            Gameweek gameweekProvider = new Gameweek(currentGameweekNumber, players, teams);
+            currentGameweekData = gameweekProvider.getCurrentGameweekData();
+        } catch (IOException ioException) {
+            LOGGER.error("Error getting current gameweek data: " + ioException.getMessage());
+        }
         FileWriter.writeData(currentGameweekData, String.format("%s%s.csv", GAMEWEEK_FILENAME, currentGameweekNumber));
 
         /* Not efficient, will change later */

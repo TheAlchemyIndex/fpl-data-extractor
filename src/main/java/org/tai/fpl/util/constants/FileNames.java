3d3a2d986a40a41eb0f2b1a21cd3ebed6b542cb1
TheAlchemyIndex <adam.vaughan@hotmail.co.uk>
2023-06-17 16:28:07 +0100

    changing file path for fixtures

--- src/main/java/org/tai/fpl/util/constants/FileNames.java
+++ src/main/java/org/tai/fpl/util/constants/FileNames.java
@@ -5,7 +5,7 @@ public class FileNames {
     public static final String TEAMS_FILENAME = "teams.csv";
     public static final String PLAYER_ID_FILENAME = "player_idlist.csv";
     public static final String GAMEWEEK_FILENAME = "gws/gw";
-    public static final String FIXTURES_FILENAME = "fixtures/fixtures.csv";
+    public static final String FIXTURES_FILENAME = "fixtures.csv";
     public static final String MERGED_GAMEWEEK_FILENAME = "merged_gw.csv";
 
     public static final String JOINED_SEASONS_FILENAME = "seasons_%s-%s.csv";
